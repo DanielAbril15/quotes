a7d7281681f5c6c479d9f35eceebd1dab0894938
Daniel Abril <danbril1015@gmail.com>
2022-07-24 11:20:30 -0500

    fin citas aleatorias

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import QuotesBox from "./components/QuotesBox";
 import quotes from "./json/quotes.json";
 import { useState } from "react";
 import CardContainer from "./components/CardContainer";
+import colors from "./utils/colors";
 
 function App() {
   const random = (array) => {
@@ -10,18 +11,30 @@ function App() {
     return array[iRandom];
   };
   let quoteRandom = random(quotes);
-
+  let colorRandom = random(colors);
   const [quoteChange, setquoteChange] = useState(quoteRandom);
+  const [colorChange, setcolorChange] = useState(colorRandom);
+
+  const obStyle = {
+    backgroundColor: colorRandom,
+  };
 
   const changeAll = () => {
     let quoteRandom = random(quotes);
+    let colorRandom = random(colors);
     setquoteChange(quoteRandom);
+    setcolorChange(colorRandom);
   };
 
   return (
-    <div className="App">
+    <div className="App" style={obStyle}>
       <QuotesBox>
-        <CardContainer quoteRandom={quoteRandom} changeAll={changeAll} />
+        <CardContainer
+          quoteRandom={quoteRandom}
+          changeAll={changeAll}
+          colorRandom={colorRandom}
+          obStyle={obStyle}
+        />
       </QuotesBox>
     </div>
   );
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-const CardContainer = ({ quoteRandom, changeAll }) => {
+const CardContainer = ({ quoteRandom, changeAll, colorRandom, obStyle }) => {
+  const color = {
+    color: colorRandom,
+  };
+
   return (
     <>
-      <div className="card__quote">
+      <div className="card__quote" style={color}>
         <i className="fa-solid fa-quote-left"></i>
         <p>{quoteRandom.quote}</p>
       </div>
-      <p className="card__autor">{quoteRandom.author}</p>
-      <button onClick={changeAll} className="card__button">
+      <p className="card__autor" style={color}>
+        {quoteRandom.author}
+      </p>
+      <button onClick={changeAll} className="card__button" style={obStyle}>
         &#62;
       </button>
     </>
